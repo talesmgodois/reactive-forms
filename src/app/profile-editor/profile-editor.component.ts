154a738595ee26ee039f903c668a36d9c536a7e3
Tales Marinho Godois <tales.godois@gmail.com>
2019-05-23 17:19:46 -0300

    nested forms

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -7,10 +7,25 @@ import { FormGroup, FormControl } from "@angular/forms";
   styleUrls: ["./profile-editor.component.sass"],
 })
 export class ProfileEditorComponent {
-  public profileForm = new FormGroup({
+  public profileForm: FormGroup = new FormGroup({
     firstName: new FormControl(""),
     lastName: new FormControl(""),
+    address: new FormGroup({
+      street: new FormControl(""),
+      city: new FormControl(""),
+      state: new FormControl(""),
+      zip: new FormControl(""),
+    }),
   });
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: "Nancy",
+      address: {
+        street: "123 Drew Street",
+      },
+    });
+  }
 
   onSubmit() {
     console.warn(this.profileForm.value);
